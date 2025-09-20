cd99abddd86cf0bafbd8cf7e0364060a363bf36a
ajaysinghraghav22 <rajputajay5450@gmail.com>
2025-09-20 15:01:41 +0530

    Added important Java coding question code

--- printdateinFormat.java
+++ printdateinFormat.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
+import java.text.SimpleDateFormat;
 public class printdateinFormat {
     public static void main(String[] args) {
-        System.out.println();
+       String format ="MM dd yy";
+       SimpleDateFormat dateformat = new SimpleDateFormat(format);
+       Date date = new Date();// Get current date
+       System.out.println(dateformat.format(date));
     }
 }
